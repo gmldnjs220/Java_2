108c3e9729d6d44874a892f9df6cc2e44e1631f7
문희원 <heewon3589@gmail.com>
2026-03-25 15:14:09 +0900

    메소드 호출 및 main 메소드 실행

--- src/Foo.java
+++ src/Foo.java
@@ -1,7 +1,21 @@
 public class Foo {
+    //두 정수를 더하는 메소드
+    public static int sum(int n, int m) {
+        return n + m;
+    }
+    
+    // main() 메소드에서 실행이 시작
     public static void main(String[] args) {
 
-        System.out.println("hellow world");
+        int i = 20;
+        int s;
+        char a;
+
+        s = sum(i, 10);
+        a = '?';
+        System.out.println(a);
+        System.out.println("hello");
+        System.out.println(s);
         
     }
 }
